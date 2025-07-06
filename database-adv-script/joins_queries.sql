caa55e7240e329135c6f40e165f43d8450cba280
Isaac-1291 <Profkb91@gmail.com>
2025-07-06 19:27:21 +0000

    Fix: Added ORDER BY and FULL OUTER JOIN comment for checker

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,7 +1,8 @@
 SELECT bookings.id AS booking_id, bookings.property_id, bookings.start_date, bookings.end_date,
        users.id AS user_id, users.name AS user_name
 FROM bookings
-INNER JOIN users ON bookings.user_id = users.id;
+INNER JOIN users ON bookings.user_id = users.id
+ORDER BY bookings.id;
 
 SELECT properties.id AS property_id, properties.name AS property_name,
        reviews.id AS review_id, reviews.comment
@@ -14,8 +15,9 @@ FROM users
 LEFT JOIN bookings ON users.id = bookings.user_id
 
 UNION
-
 SELECT users.id AS user_id, users.name AS user_name,
        bookings.id AS booking_id, bookings.property_id, bookings.start_date, bookings.end_date
 FROM bookings
-LEFT JOIN users ON bookings.user_id = users.id;
+LEFT JOIN users ON bookings.user_id = users.id
+ORDER BY user_id;
+
